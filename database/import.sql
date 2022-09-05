74c2763a7abee9ba973ba8ae33040744953ef456
bitwessel <101328841+bitwessel@users.noreply.github.com>
2022-09-05 16:26:52 +0200

    Update import.sql
    tabelcoaches

--- database/import.sql
+++ database/import.sql
@@ -12,3 +12,14 @@ CREATE TABLE IF NOT EXISTS `students` (
 
 INSERT INTO students (username, password, email)
 	VALUES ('Diana Kasten', '734-887-7989', 'DianaMKasten@jourrapide.com')
+
+CREATE TABLE IF NOT EXISTS `coaches` (
+    `id` int PRIMARY KEY AUTO_INCREMENT ,
+    `username` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+INSERT INTO coaches (username, password, email)
+    VALUES ('Ties', '940-211-3429', 'ties@bit-academy.nl')
