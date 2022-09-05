bb262b1008ab2063ae6f8dd659a3a3cfbaf61612
bitwessel <101328841+bitwessel@users.noreply.github.com>
2022-09-05 16:44:10 +0200

    Update import.sql
    hintstabel

--- database/import.sql
+++ database/import.sql
@@ -6,20 +6,26 @@ CREATE TABLE IF NOT EXISTS `students` (
     `id` int PRIMARY KEY AUTO_INCREMENT ,
     `username` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+    `email` varchar(255) NOT NULL
+);
 
 INSERT INTO students (username, password, email)
-	VALUES ('Diana Kasten', '734-887-7989', 'DianaMKasten@jourrapide.com')
+	VALUES ('Diana Kasten', '734-887-7989', 'DianaMKasten@jourrapide.com');
 
 CREATE TABLE IF NOT EXISTS `coaches` (
     `id` int PRIMARY KEY AUTO_INCREMENT ,
     `username` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+    `email` varchar(255) NOT NULL
+);
 
 INSERT INTO coaches (username, password, email)
-    VALUES ('Ties', '940-211-3429', 'ties@bit-academy.nl')
+    VALUES ('Ties', '940-211-3429', 'ties@bit-academy.nl');
+
+CREATE TABLE IF NOT EXISTS `hints` (
+    `hint_id` int PRIMARY KEY AUTO_INCREMENT ,
+    `hint` TEXT NOT NULL
+);
+
+
+
