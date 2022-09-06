411b749b373b7e84fe5ecdac4feb4bb664262536
DESKTOP-UPQT4RS\Iz-Dine <Izdinea1234gmail.com>
2022-09-06 16:19:36 +0200

    profile pagina link button in header

--- profile.php
+++ profile.php
@@ -21,7 +21,7 @@ $data = new data()?>
 
 
 
-  <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+<div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
     
 	<!--Main Col-->
 	<div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
