ab39b68c3f923dcc297b3fd408d33f467e21e0ce
bitwessel <101328841+bitwessel@users.noreply.github.com>
2022-09-06 10:29:09 +0200

    database
    koppeltabel gefixt

--- database/import.sql
+++ database/import.sql
@@ -32,13 +32,20 @@ CREATE TABLE IF NOT EXISTS `opdrachten` (
     `opdracht` TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `opdrachten_hints` (
-    `opdracht_id` int NOT NULL,
-    `hint_id` int NOT NULL,
-    PRIMARY KEY (`opdracht_id`, `hint_id`),
-    FOREIGN KEY (`opdracht_id`) REFERENCES `opdrachten` (`opdracht_id`),
-    FOREIGN KEY (`hint_id`) REFERENCES `hints` (`hint_id`)
+INSERT INTO opdracht (opdracht)
+    VALUES ('Maak een database aan met de naam WHIM');
+
+INSERT INTO hints (hint)
+    VALUES ('Google: create database');
+
+CREATE TABLE opdrachten_hints(
+    opdrachten_id INT NOT NULL,
+    hint_id INT NOT NULL,
+    FOREIGN KEY (opdrachten_id) REFERENCES opdrachten(opdracht_id),
+    FOREIGN KEY (hint_id) REFERENCES hints(hint_id)
 );
 
+INSERT INTO opdrachten_hints (opdracht_id, hint_id)
+    VALUES (1, 1);
 
 
