1fc5bdf16d5692204ec112da76f984bf8f2baf3b
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-09-06 10:54:56 +0200

    Update data.php

--- data.php
+++ data.php
@@ -1,7 +1,7 @@
 <?php
 require 'database_connect.php';
 class data {
-    //Hier wordt bestaande data opgehaald
+    //Hier wordt bestaande data opgehaald van studenten die ook vast zitten
     function get_stuck_students() {
         global $conn;
          return $conn->query('SELECT `username` FROM students')->fetchAll();
