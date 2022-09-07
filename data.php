186cacb0d0ff7ab80b5bb6f9a4963e0724f6e1ba
Mark Verrips <2083591@talnet.nl>
2022-09-07 14:45:48 +0200

    aanwezige coaches fix

--- data.php
+++ data.php
@@ -1,31 +1,37 @@
 <?php
+
 require 'database_connect.php';
-class data {
+class data
+{
+
+
     //Hier wordt bestaande data opgehaald van studenten die ook vast zitten
-    function get_stuck_students() {
+    function get_stuck_students()
+    {
         global $conn;
-        return conn->query('SELECT `username` FROM students')->fetchAll();
+        return $conn->query('SELECT `username` FROM students')->fetchAll();
     }
-    function get_coach() {
+    function get_coach()
+    {
         global $conn;
         return $conn->query('SELECT * FROM coaches')->fetchAll();
     }
-    function status_change() {
+    function status_change()
+    {
         global $conn;
         $query = "UPDATE stuck_students SET status = ? WHERE id = ?";
         $conn->prepare($query)->execute([]);
     }
-    function list_stuck_students() {
+    function list_stuck_students()
+    {
         global $conn;
-        $data = $conn->query(
-            "SELECT stuck_students.id, students.username, opdrachten.opdracht, stuck_students.status
+        $data = $conn->query("SELECT stuck_students.id, students.username, opdrachten.opdracht, stuck_students.status
         FROM stuck_students
         INNER JOIN students ON stuck_students.id = students.id
         INNER JOIN opdrachten ON stuck_students.id = opdrachten.id;")->fetchAll();
         foreach ($data as $list) {
             $status_html = ($list["status"] == 1) ? '<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>' : '<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-gray-800">Not Active</span>' ;
-
-             echo <<<HTML
+            echo <<<HTML
                             <tr>
                                 <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                     <div class="flex items-center">
@@ -56,34 +62,39 @@ class data {
 HTML;
         }
     }
-    function availableCoach() { 
+    function availableCoach()
+    {
         $coaches = $this->get_coach();
         $currentday = date('l');
         $checkAvailable;
-        switch($currentday) 
-        {
+        switch ($currentday) {
             case 'Monday':
                 $checkAvailable = 'ma';
+
                 break;
             case 'Tuesday':
                 $checkAvailable = 'di';
+
                 break;
             case 'Wednesday':
                 $checkAvailable = 'wo';
+
                 break;
             case 'Thursday':
                 $checkAvailable = 'do';
+
                 break;
             case 'Friday':
                 $checkAvailable = 'vr';
+
                 break;
         }
-
+        $checkAvailable = "di";
         foreach ($coaches as $coach) {
             if (strpos($coach['available_days'], $checkAvailable) !== false) {
-                echo $coach['username'];
-                echo $coach['image_url'];
+                echo "<li class='inline-flex mb-4 whitespace-pre-wrap'>" . $coach['username'];
+                echo "<img src=" . $coach['image_url'] . " height='20' width='20'></li><br>";
             }
         }
-    }    
+    }
 }
